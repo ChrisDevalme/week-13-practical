7dba474be649491db0cbc4d0f10b7b298bd6b6d6
Chris Devalme <cdevalme@gmail.com>
2023-12-26 00:41:45 -0500

    Magic 8 Ball express application created.

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -24,6 +24,14 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     
 })
 
+app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res) => {
+    const magicResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+    const randomResponse = Math.floor(Math.random() * magicResponses.length)
+    res.send(`<h1>${magicResponses[randomResponse]}</h1>`)
+})
+
+
 
 app.listen(3000, () => {
     console.log("I'm ready")
