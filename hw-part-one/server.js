ff99791b24db9030a97382f77d445ba060e9fd7d
Chris Devalme <cdevalme@gmail.com>
2023-12-25 22:29:46 -0500

    Greeting express application created

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -1,5 +1,23 @@
 const express = require('express') // imported express from node modules
+const jsxEngine = require('jsx-view-engine')
 const app = express()
+
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/greeting', (req, res) => {
+    res.send('<h1>Hello Stranger</h1>')
+})
+
+app.get('/greeting', (req, res) => {
+    res.send('<h1>Hello Stranger</h1>')
+})
+
+app.get('/greeting/:name', (req, res) => {
+    res.send(`Hello ${req.params.name} It's great to see you!`)
+})
+
+
 
 app.listen(3000, () => {
     console.log("I'm ready")
