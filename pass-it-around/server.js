d35735f9a9b372e1128d5c12bc8ca6e5a395c7bd
Chris Devalme <cdevalme@gmail.com>
2023-12-26 21:02:06 -0500

    Pass it Around Complete

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -1,2 +1,27 @@
 const express = require('express') // imported express from node modules
 const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send(
+        `<h1>99 Little Bugs in the Code</h1>
+        <a href="/98">take one down patch it around</a>`)
+})
+
+app.get('/:number_of_bugs', (req, res) => {
+    let bugs = req.params.number_of_bugs
+    if (bugs <= 0) {
+        res.send(
+                `<h1>0 Bugs Left</h1>
+                <a href="/">Start Over</a>`)
+    } else {
+    res.send(
+        `<h1>${bugs} Little Bugs in the Code</h1>
+        <a href=${bugs - 1}>take one down patch it around</a>`        
+        )  }
+})
+
+app.listen(3000, () => {
+    console.log("I'm ready")
+})
